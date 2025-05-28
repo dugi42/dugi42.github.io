553773f3ed7ae2535e06caaa79ca79590a83b318
Daniel Hauser <daniel.hauser@datalabhell.at>
2025-05-28 11:59:40 +0200

    deploy test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748425335',resources: [
+    cacheName: 'chirpy-1748426361',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
