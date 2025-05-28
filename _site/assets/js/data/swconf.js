42630dac26771c75c280b55c84957c24e36a7863
Daniel Hauser <daniel.hauser@datalabhell.at>
2025-05-28 15:06:24 +0200

    update layout

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748437405',resources: [
+    cacheName: 'chirpy-1748437524',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
