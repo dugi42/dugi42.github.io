b4f0e8ca580ee8a33e52d7e10617a47f37f3438a
Daniel Hauser <danielhauser@Mac.fritz.box>
2025-08-24 19:18:11 +0200

    google analytics added

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756055711',resources: [
+    cacheName: 'chirpy-1756055865',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
