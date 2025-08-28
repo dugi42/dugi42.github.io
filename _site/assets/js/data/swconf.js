48579a01e0502a0e36c56a2d23a5bba924ead55f
Daniel Hauser <danielhauser@Mac.fritz.box>
2025-08-28 13:22:17 +0200

    article updated

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756055865',resources: [
+    cacheName: 'chirpy-1756380127',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
